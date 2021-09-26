43dfd8c05280922ec7e91610405f912dffc9bb94
Václav Lipert <vlipert@seznam.cz>
2021-09-26 12:56:37 +0200

    Update to 1.213 version

--- foe/request.py
+++ foe/request.py
@@ -50,19 +50,17 @@ class Request(object):
         headers = {
             "Connection": "keep-alive",
             # Important to keep this up to date
-            "Client-Identification": "version=%s; requiredVersion=%s; platform=bro; platformVersion=web" % (version, version),
-            "Origin": "https://foeen.innogamescdn.com",
-            "X-Requested-With": "ShockwaveFlash/26.0.0.131",
+            "Client-Identification": "version=%s; requiredVersion=%s; platform=bro; platformType=html5; platformVersion=web" % (version, version),
+            "Origin": "https://%s.forgeofempires.com" % (config['game']['server']),
             "Signature": "%s" % signature,
             "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
             "Content-Type": "application/json",
             "Accept": "*/*",
-            "DNT": "1",
-            "Referer": "https://foeen.innogamescdn.com/swf/Preloader.swf?%s/[[DYNAMIC]]/1" % (config['game']['timestamp']),
+            "Referer": "https://%s.forgeofempires.com/game/index?" % (config['game']['server']),
             "Host": "%s.forgeofempires.com" % (config['game']['server']),
-            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Encoding": "gzip, deflate",
             "Accept-Language": "en-US,en;q=0.8,ja;q=0.6",
-            "Cookie": "metricsUvId=98cf6fd3-5872-4080-bf40-934b90b70a71; sid=%s; req_page_info=game_v1; start_page_type=game; start_page_version=v1; _ga=GA1.2.1298412460.1491422581; ig_conv_last_site=https://%s.forgeofempires.com/game/index" % (config['login']['sid'], config['game']['server']),
+            "Cookie": "metricsUvId=22af0da5-abfe-4178-ba80-09c0fb87d145; sid=%s; ig_conv_last_site=https://%s.forgeofempires.com/game/index" % (config['login']['sid'], config['game']['server']),
         }
 
         url = 'https://%s.forgeofempires.com/game/json?h=%s' % (config['game']['server'], config['login']['user_key'])
